0ea36ee3ffcaecd58b4d3e87f2226727afa669a0
shrawani maharjan <shrawanimaharjan@gmail.com>
2023-04-11 21:00:00 +0545

    Added

--- script.js
+++ script.js
@@ -9,14 +9,13 @@ async function checkWeather(city = "Birendranagar") {
     const response = await fetch(apiUrl);
     const data = await response.json();
     console.log(data);
+    saveWeather(data);
 
-    const currentDate = new Date();
-    const day = getDayString(currentDate.getDay());
-    const date = currentDate.getDate();
-    const month = getMonthString(currentDate.getMonth());
+    const currentDate = convertTimeStamp(data.dt);
+    
 
     document.querySelector(".city").textContent = data.name;
-    document.querySelector(".date").textContent = `${day}, ${date} ${month}`;
+    document.querySelector(".date").textContent = currentDate;
     document.querySelector(".temp").innerHTML = `${data.main.temp}°C`;
     document.querySelector(".condition").textContent = data.weather[0].description;
     document.querySelector(".humidity").textContent = `${data.main.humidity}%`;
@@ -30,7 +29,34 @@ async function checkWeather(city = "Birendranagar") {
     console.error("Error fetching weather data:", error);
   }
 }
+function viewData(cityname){
+  console.log("VIEWDATA: Running")
+  console.log(cityname);
+  var form = document.createElement('form');
+  form.method = 'POST';
+  form.action = 'retrive.php';
+  form.target = '_blank';  // to open in a new tab
 
+  var hiddenField = document.createElement('input');
+  hiddenField.type = 'hidden';
+  hiddenField.name = 'cityval';
+  hiddenField.value = cityname;
+
+  form.appendChild(hiddenField);
+  document.body.appendChild(form);
+
+  form.submit();
+
+  document.body.removeChild(form);
+}
+
+function convertTimeStamp(timestamp) {
+  const date = new Date(timestamp * 1000);
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); 
+  const day = date.getDate().toString().padStart(2, '0');
+  return `${year}-${month}-${day}`;
+}
 function getDayString(dayIndex) {
   const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   return days[dayIndex];
@@ -49,8 +75,8 @@ function getWeatherIconUrl(iconCode) {
     "02": "icons/partly_cloudy.png",
     "03": "icons/cloudy.png",
     "04": "icons/cloudy.png",
-    "09": "icons/rain.png",
-    "10": "icons/rain.png",
+    "09": "icons/rainy.png",
+    "10": "icons/rainy.png",
     "11": "icons/thunderstorm.png",
     "13": "icons/snowy.png",
     "50": "icons/mist.png",
@@ -59,6 +85,45 @@ function getWeatherIconUrl(iconCode) {
   const weatherIconCode = iconCode.substr(0, 2);
   const weatherIconUrl = iconMap[weatherIconCode] || "icons/default.png";
   return weatherIconUrl;
+}
+async function sendWeather (data) {
+  try {
+    let date = convertTimeStamp(data.dt);
+
+    let weatherData = {
+      city: data.name,
+      country: data.sys.country,
+      date: date,
+      weatherCondition: data.weather[0].description,
+      weatherIcon: data.weather[0].icon,
+      temperature: data.main.temp,
+      pressure: data.main.pressure,
+      windSpeed: data.wind.speed,
+      humidity: data.main.humidity,
+    };
+
+    console.log(weatherData);
+    const url = "data.php";
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify(weatherData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const response = await fetch(url, options);
+
+    if (response.ok) {
+      const responseData = await response.text();
+      console.log(responseData);
+    } else {
+      console.log("Error: " + response.status);
+    }
+  } catch (error) {
+    console.log("An error occurred:", error);
+  }
 }
 
 // Add event listener to the search button
